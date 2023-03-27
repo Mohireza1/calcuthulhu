ad6fa6f78f4f361c493de6de5cc3da3945e6cbe9
Mohireza <mohireza50@gmail.com>
2023-03-27 14:14:35 +0430

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+  insert number
+    number is shown in gray
+    number is saved
+
+    select one of the four (sum)
+    function flag = sum
+
+    another number inserted
+    
+    if (another function) the numbers are mixed and the final result is saved and gray and flag is function
+    if (equals) the numbers are mixed and the final result is black and saved and flag is fasle
+    */
