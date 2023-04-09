577ae585fd0cedf12b99b628ac2da0a169586ea1
Mohireza <mohireza50@gmail.com>
2023-04-09 14:56:42 +0430

    + and - and % and +/- and backspace ready

--- script.js
+++ script.js
@@ -1,5 +1,120 @@
 'use strict';
 
+// Query selectors
+
+const calculator = document.querySelector('.calculator');
+const calculatorInput = document.querySelector('.calculator__input');
+const calculatorButtons = [...document.querySelectorAll('.calculator__button')];
+
+// Variables
+
+let flag = false;
+let currentNumString = '';
+let savedNumString = 0;
+
+// Functions
+
+const backspace = () => {
+  if (currentNumString[0] === '-' && currentNumString.length === 2) {
+    currentNumString = '';
+    calculatorInput.textContent = '0';
+  } else if (currentNumString.length === 1) {
+    currentNumString = '';
+    calculatorInput.textContent = '0';
+  } else if (!currentNumString) {
+    return;
+  } else {
+    currentNumString = currentNumString.slice(0, currentNumString.length - 1);
+    calculatorInput.textContent = currentNumString;
+  }
+};
+
+const percent = () => {
+  if (
+    (flag === 'sum' && currentNumString) ||
+    (flag === 'minus' && currentNumString)
+  ) {
+    currentNumString = `${(currentNumString * savedNumString) / 100}`;
+    calculatorInput.textContent = currentNumString;
+    console.log(currentNumString);
+  } else if (currentNumString) {
+    currentNumString = `${currentNumString / 100}`;
+    calculatorInput.textContent = currentNumString;
+    console.log(currentNumString);
+  }
+};
+
+const negative = () => {
+  currentNumString = `${currentNumString * -1}`;
+  calculatorInput.textContent = currentNumString;
+};
+
+const plus = () => {
+  savedNumString += +currentNumString;
+  if (flag) {
+    currentNumString = '';
+    calculatorInput.textContent = `${savedNumString}`;
+  } else {
+    currentNumString = '';
+    calculatorInput.textContent = '0';
+  }
+  flag = 'plus';
+};
+const minus = () => {
+  savedNumString -= +currentNumString;
+  if (flag) {
+    currentNumString = '';
+    calculatorInput.textContent = `${savedNumString}`;
+  } else {
+    currentNumString = '';
+    calculatorInput.textContent = '0';
+  }
+  flag = 'minus';
+};
+
+// Eventlisteners
+
+calculatorInput.addEventListener('focusout', () => {
+  setTimeout(() => {
+    calculatorInput.focus();
+  }, 10);
+});
+
+window.addEventListener('keydown', e => {
+  console.log(e);
+
+  if (+e.key) {
+    currentNumString += e.key;
+    calculatorInput.textContent = currentNumString;
+  }
+  if (e.key === 'Backspace') backspace();
+});
+
+calculator.addEventListener('click', e => {
+  if (!e.target.classList.contains('calculator__button')) return;
+
+  const cls = e.target.classList;
+
+  switch (true) {
+    case cls.contains('calculator__button--back'):
+      backspace();
+      break;
+
+    case cls.contains('calculator__button--percent'):
+      percent();
+      break;
+    case cls.contains('calculator__button--negative'):
+      negative();
+      break;
+    case cls.contains('calculator__button--plus'):
+      plus();
+      break;
+    case cls.contains('calculator__button--minus'):
+      minus();
+      break;
+  }
+});
+
 /*
   insert number
     number is shown in gray
